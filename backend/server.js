34ff766473445964c94e39cbdab43adb66451d18
tli533 <tli533@aucklanduni.ac.nz>
2024-03-08 16:26:53 +1100

    Creating Express router and API routes
    workout routes for the api and testing them out using POSTMAN

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,30 @@
 require('dotenv').config()
 
 const express = require('express')
+const workoutRoutes = require('./routes/workouts')
 
 //express app
 const app = express()   
 
 //middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
 //routes
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'})
-})
+app.use('/api/workouts', workoutRoutes)
+
+//testing the api
+// app.get('/', (req, res) => {
+//     res.json({mssg: 'Welcome to the app'})
+// })
 
 //listejn for requests
 app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.Port)
+    console.log('listening on port', process.env.PORT)
 })
 
-process.env
+//process.env
