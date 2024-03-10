c987e2705009b145b3c95214ac0dcf38ac4bd42a
tli533 <tli533@aucklanduni.ac.nz>
2024-03-10 13:11:32 +1100

    Creating workout/js
    preparing for models and schema

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     //listening for request
     app.listen(process.env.PORT, () => {
-        console.log('listening on port', process.env.PORT)
+        console.log('connected to db & listening on port', process.env.PORT)
     })
 })
 .catch((error) => {
