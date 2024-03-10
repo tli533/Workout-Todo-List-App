b57eec518700b3bb5c724fe69bace0dedc0cce9a
tli533 <tli533@aucklanduni.ac.nz>
2024-03-10 13:08:10 +1100

    Added mongoose
    Connecting to mongodb atlas and mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 //express app
@@ -17,14 +18,24 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/workouts', workoutRoutes)
 
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    //listening for request
+    app.listen(process.env.PORT, () => {
+        console.log('listening on port', process.env.PORT)
+    })
+})
+.catch((error) => {
+    console.log(error)
+})
+
 //testing the api
 // app.get('/', (req, res) => {
 //     res.json({mssg: 'Welcome to the app'})
 // })
 
 //listejn for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
-})
+
 
 //process.env
