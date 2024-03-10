3f5fde1850a64671ae1322458368509afe8b61a8
tli533 <tli533@aucklanduni.ac.nz>
2024-03-10 16:31:23 +1100

    Created deleteworkout function
    deletes workout

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -40,12 +40,27 @@ const createWorkout = async (req, res) => {
 }
 
 //delete a workout
+const deleteWorkout = async (req, res) => {
+    const { id } = req.params
 
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    const workout = await WorkoutModel.findOneAndDelete({_id: id})
+
+    if (!workout) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    res.status(200).json(workout)
+}
 //update a workout
 
 
 module.exports = {
     getWorkouts,
     getWorkout,
-    createWorkout
+    createWorkout,
+    deleteWorkout
 }
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -2,7 +2,8 @@ const express = require('express')
 const {
     createWorkout,
     getWorkout,
-    getWorkouts
+    getWorkouts,
+    deleteWorkout
 } = require('../controllers/workoutController')
 
 //creating router instance
@@ -19,9 +20,7 @@ router.get('/:id', getWorkout)
 router.post('/', createWorkout)
 
 //DELETE a workout
-router.delete('/:id', (req, res) => {
-    res.json({messg: 'DELETE a workout'})
-})
+router.delete('/:id', deleteWorkout)
 
 //UPDATE a workout
 router.patch('/:id', (req, res) => {
