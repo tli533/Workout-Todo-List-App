c88e2b245bfa88e9271b9711550ac0ddb54e672e
tli533 <tli533@aucklanduni.ac.nz>
2024-03-12 16:58:43 +1100

    Able to fetch data

---
+++ frontend/src/components/WorkoutDetails.js
@@ -0,0 +1,12 @@
+const WorkoutDetails = ({ workout }) => {
+    return (
+        <div classNmae="workout-details">
+            <h4>{workout.title}</h4>
+            <p><strong>Load (kg): </strong>{workout.load}</p>
+            <p><strong>Reps: </strong>{workout.reps}</p>
+            <p>{workout.createdAt}</p>
+        </div>
+    )
+} 
+
+export default WorkoutDetails
