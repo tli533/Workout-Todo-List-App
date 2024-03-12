bdd0fad58db431fea7ebc9fb9cd6b44b528d20a0
tli533 <tli533@aucklanduni.ac.nz>
2024-03-12 17:08:49 +1100

    Created workout form file
    Where we can create new workout forms

---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,11 @@
+import {useState } from "react"
+
+const WorkoutForm = () => {
+    const [title, setTitle] = useState('')
+    const [load, setLoad] = useState('')
+    const [reps, setReps] = useState('')
+
+    return {
+
+    }
+}
