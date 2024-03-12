00bb1d23f68c5b29499cb2012f3248bcfdefa39e
tli533 <tli533@aucklanduni.ac.nz>
2024-03-12 17:24:38 +1100

    added form and error handler

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -4,8 +4,60 @@ const WorkoutForm = () => {
     const [title, setTitle] = useState('')
     const [load, setLoad] = useState('')
     const [reps, setReps] = useState('')
+    const [error, setError] = useState(null)
 
-    return {
+    const handleSubmit = async (e) => {
+        e.preventDefault()
 
+        const workout = {title, load, reps} //the function will change it to a json string
+
+        const response = await fetch('/api/workouts', {
+            method: 'POST',
+            body: JSON.stringify(workout),
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        })
+        const json = await response.json()
+
+        if (!response.ok) {
+            setError(json.error)
+        }
+
+        if (response.ok) {
+            
+            setError(null)
+            console.log('new workout added', json)
+        }
     }
+
+    return (
+        <form className="create" onSubmit={handleSubmit}>
+            <h3>Add a New Workout</h3>
+
+            <label>Excerise Title:</label>
+            <input 
+            type="text"
+            onChange={(e) => setTitle(e.target.value)} 
+            value={title}
+            />
+
+            <label>Load (Kg):</label>
+            <input 
+            type="number"
+            onChange={(e) => setLoad(e.target.value)} 
+            value={load}
+            />
+
+            <label>Reps:</label>
+            <input 
+            type="number"
+            onChange={(e) => setReps(e.target.value)} 
+            value={Reps}
+            />
+
+            <button>Add Workout</button>
+        </form>
+    )
+        
 }
