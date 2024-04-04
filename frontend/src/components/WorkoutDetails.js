9ed6ebcd5031ae68ccf52f3fb8364c7b28601ec8
tli533 <tli533@aucklanduni.ac.nz>
2024-04-04 14:58:02 +1100

    Inline editable input fields WIP

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -4,8 +4,12 @@ import { Link} from 'react-router-dom';
 
 
 const WorkoutDetails = ({ workout }) => {
+    
     const { dispatch } = useWorkoutsContext()
-    const [tempTitle, setTempTitle ] =useState()
+    const [tempTitle, setTempTitle] =useState(workout.title)
+    const [tempLoad, setTempLoad] = useState(workout.load)
+    const [tempReps, setTempReps] = useState(workout.reps)
+
 
     //handles deleting workouts
     const handleClick = async () => {
@@ -19,15 +23,43 @@ const WorkoutDetails = ({ workout }) => {
         }
     }
 
+    //handles updating temporary title
+    const handleTitleChange = (e) => {
+        setTempTitle(e.target.value);
+    }
+
+    const handleLoadChange = (e) => {
+        setTempLoad(e.target.value);
+    }
+
+    const handleRepsChange = (e) => {
+        setTempReps(e.target.value);
+    }
+
+
+
     return (
         <div className="workout-details">
-            <input className="block"
+            <h4>Title</h4><input className="title-detail"
             type = "text"
-            value={workout.title}/>
-            <p><strong>Load (kg): </strong>{workout.load}</p>
-            <p><strong>Reps: </strong>{workout.reps}</p>
+            value={tempTitle}
+            onChange={handleTitleChange}
+            />
+            
+            <p><strong>Load (kg): </strong></p>
+            <input className="load-detail"
+            type = "number"
+            value={tempLoad}
+            onChange={handleLoadChange}
+            />
+            <p><strong>Reps: </strong></p>
+            <input className="rep-detail"
+            type = "number"
+            value={tempReps}
+            onChange={handleRepsChange}
+            />
             <p>{workout.createdAt}</p>
-            <span onClick={handleClick}>delete</span>
+            <span onClick={handleClick}>Delete</span>
             <Link to={`/${workout._id}`}><button>edit</button></Link>
             
         </div>
