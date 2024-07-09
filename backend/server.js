7f02c329009dd2b89510c3256f8a69bdb74f214d
tli533 <tli533@aucklanduni.ac.nz>
2024-07-09 15:46:17 +1000

    using different app.use

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,8 @@ app.use((req, res, next) => {
 })
 
 //routes
-app.use('/api/workouts', workoutRoutes)
-//app.use('https://learning-mern-stack-s.vercel.app/api/workouts', workoutRoutes)
+//app.use('/api/workouts', workoutRoutes)
+app.use('https://learning-mern-stack-s.vercel.app/api/workouts', workoutRoutes)
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
