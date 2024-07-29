0ac529ece5a4ce30c4e3543159f2dc0825ff4561
tli533 <tli533@aucklanduni.ac.nz>
2024-07-30 02:49:43 +1000

    Revert "Removing unused code"
    This reverts commit edf79138615bfbd6ed2ffa323210e8bc76438d25.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />}/>
-
+            <Route path="/:id" element={<Edit />}/>
           </Routes>
         </div>
       </BrowserRouter>
